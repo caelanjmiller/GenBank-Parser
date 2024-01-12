0d79f32936759f6f80351bcb2721671657f4d02d
Caelan Miller <98186985+caelanjmiller@users.noreply.github.com>
2024-01-12 16:05:35 -0600

    Added Sequence Assignment fxn

--- genbank_parser.py
+++ genbank_parser.py
@@ -32,13 +32,18 @@ class GenBank:
         def annotate_cds(self, file_contents: list) -> object:
             # Capture indices at which gene feature key occurs in GenBank file content list
             gene_feature_indices: list = [index for index, value in enumerate(file_contents) if value.startswith('gene')]
+            # Capture index at which ORIGIN occurs - signals end of GenBank annotation before full sequence
+            origin_index: int = [index for index, value in enumerate(file_contents) if value.startswith('ORIGIN')][0]
             # Create a list containing tuples of the indices of the information contained between each occurrence of gene feature keys
-            information_indices: list = [tuple((gene_feature_indices[index] + 1, gene_feature_indices[index])) for index in range(len(gene_feature_indices))]
+            information_indices: list = [tuple((gene_feature_indices[index] + 1, gene_feature_indices[index + 1])) for index in range(len(gene_feature_indices) - 1)]
+            # Capture last gene entry prior to ORIGIN
+            information_indices.append(tuple((gene_feature_indices[-1], origin_index)))
             parsed_file_contents: list = [list(islice(file_contents, a, b)) for a, b in information_indices]
             for gene_slice in parsed_file_contents:
                 gene = Gene()
                 gene.assign_annotated_function(gene_slice)
                 gene.assign_protein_id(gene_slice)
+                gene.assign_sequence(gene_slice)
                 self.genes.append(gene)
 
 
@@ -150,4 +155,14 @@ class Gene:
         pass
 
     def assign_sequence(self, file_contents: list) -> str:
-        pass
+        for gene_entry in file_contents:
+            if 'translation' in gene_entry:        
+                translation_feature_index: int = [index for index, value in enumerate(file_contents) if 'translation' in value][0]
+                lines_after_translation: list = []
+                for line in file_contents[translation_feature_index:]:
+                    lines_after_translation.append(line)
+                # Join list of translation sequence together into string and format by removing whitespace and quotations
+                sequence: str = "".join(lines_after_translation).split('/translation=')[1].strip().replace('"', "")
+                self.sequence = sequence
+            else:
+                continue
